76174d7a8ce0ae3464cb2c4ac09a91509ea929c0
Valentin Placido <valentinplacido25@gmail.com>
2020-01-24 04:18:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,6 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   newData = {}
   data.each do |attribute, hash|
-    value.each do |attribute, arr|
+    value.each do |catagory, arr|
       
 end
