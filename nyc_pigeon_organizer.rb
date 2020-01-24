746a1f64168d234e9fccf712e0c7c0786c1a0248
Valentin Placido <valentinplacido25@gmail.com>
2020-01-24 04:28:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newData = {}
+  data.each do |attribute, hash|
+    hash.each do |catagory, arr|
+      arr.each do |value|
+        if !newData.has_key?(value)
+          newData[value] = {}
+        end
+        if !newData[value].has_key?(attribute)
+          newData[value][attribute] = []
+        end
+        if !newData[value][attribute].include?(catagory)
+          newData[value][attribute] << catagory.to_s 
+        end
+      end 
+    end 
+  end 
+  newData
 end
